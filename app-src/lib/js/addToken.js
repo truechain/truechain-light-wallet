dae2a90059e392d47d2099861ad8eead6ae98625
see91 <loverrenmin@qq.com>
2018-05-07 09:21:57 +0800

    1.0.1  增加手势功能-keystore导入导出等

--- app-src/lib/js/addToken.js
+++ app-src/lib/js/addToken.js
@@ -1,173 +1,181 @@
-var web3 = new Web3();
-
-function setWeb3Provider(keystore) {
-	var web3Provider = new HookedWeb3Provider({
-		//host: "http://localhost:8545", // 私链 
-		//host: "https://rinkeby.infura.io/",		// 以太坊测试  
-		//host: "https://ropsten.infura.io/", // 以太坊测试 (ropsten)
-		host: "https://mainnet.infura.io/", // 以太坊正式
-		transaction_signer: keystore
-	});
-	web3.setProvider(web3Provider);
-}
-
-let symbol, contract, name, decimals, balances;
-
 // 根据当前帐户地址 和 合约地址，查询代表名称、余额
 function addToken(contractAddr, callback) {
+	let symbol, contract, name, decimals, balances;
+	mui.plusReady(function() {
+		var web3 = new Web3();
+
+		let host = plus.storage.getItem('web3Host');
+		if(!host) {
+			host = 'https://mainnet.infura.io/';
+		};
+
+		function setWeb3Provider(keystore) {
+			var web3Provider = new HookedWeb3Provider({
+				//host: "http://localhost:8545", // 私链 
+				//host: "https://rinkeby.infura.io/",		// 以太坊测试  
+				//host: "https://ropsten.infura.io/", // 以太坊测试 (ropsten)
+				host: host, // 以太坊正式
+				transaction_signer: keystore
+			});
+			web3.setProvider(web3Provider);
+		}
+
+		var fromAddr = '0x' + plus.storage.getItem('walletAddress');
+		var abi = [{
+			"constant": true,
+			"inputs": [],
+			"name": "name",
+			"outputs": [{
+				"name": "",
+				"type": "string"
+			}],
+			"payable": false,
+			"type": "function"
+		}, {
+			"constant": true,
+			"inputs": [],
+			"name": "totalSupply",
+			"outputs": [{
+				"name": "supply",
+				"type": "uint256"
+			}],
+			"payable": false,
+			"type": "function"
+		}, {
+			"constant": false,
+			"inputs": [{
+				"name": "_from",
+				"type": "address"
+			}, {
+				"name": "_to",
+				"type": "address"
+			}, {
+				"name": "_value",
+				"type": "uint256"
+			}],
+			"name": "transferFrom",
+			"outputs": [{
+				"name": "success",
+				"type": "bool"
+			}],
+			"payable": false,
+			"type": "function"
+		}, {
+			"constant": true,
+			"inputs": [],
+			"name": "decimals",
+			"outputs": [{
+				"name": "",
+				"type": "uint256"
+			}],
+			"payable": false,
+			"type": "function"
+		}, {
+			"constant": true,
+			"inputs": [{
+				"name": "_owner",
+				"type": "address"
+			}],
+			"name": "balanceOf",
+			"outputs": [{
+				"name": "balance",
+				"type": "uint256"
+			}],
+			"payable": false,
+			"type": "function"
+		}, {
+			"constant": true,
+			"inputs": [],
+			"name": "symbol",
+			"outputs": [{
+				"name": "",
+				"type": "string"
+			}],
+			"payable": false,
+			"type": "function"
+		}, {
+			"constant": false,
+			"inputs": [{
+				"name": "_to",
+				"type": "address"
+			}, {
+				"name": "_value",
+				"type": "uint256"
+			}],
+			"name": "transfer",
+			"outputs": [{
+				"name": "success",
+				"type": "bool"
+			}],
+			"payable": false,
+			"type": "function"
+		}, {
+			"constant": true,
+			"inputs": [{
+				"name": "_owner",
+				"type": "address"
+			}, {
+				"name": "_spender",
+				"type": "address"
+			}],
+			"name": "allowance",
+			"outputs": [{
+				"name": "remaining",
+				"type": "uint256"
+			}],
+			"payable": false,
+			"type": "function"
+		}, {
+			"anonymous": false,
+			"inputs": [{
+				"indexed": true,
+				"name": "_from",
+				"type": "address"
+			}, {
+				"indexed": true,
+				"name": "_to",
+				"type": "address"
+			}, {
+				"indexed": false,
+				"name": "_value",
+				"type": "uint256"
+			}],
+			"name": "Transfer",
+			"type": "event"
+		}, {
+			"anonymous": false,
+			"inputs": [{
+				"indexed": true,
+				"name": "_owner",
+				"type": "address"
+			}, {
+				"indexed": true,
+				"name": "_spender",
+				"type": "address"
+			}, {
+				"indexed": false,
+				"name": "_value",
+				"type": "uint256"
+			}],
+			"name": "Approval",
+			"type": "event"
+		}]
+
+		contract = web3.eth.contract(abi).at(contractAddr);
+		let keyStore = plus.storage.getItem('keystore');
+		setWeb3Provider(keyStore);
+		symbol = contract.symbol();
+		name = contract.name();
+		//decimals = contract.decimals(); //小数点位数
+		balances = contract.balanceOf(fromAddr); //查地址的余额	
+		balances = Number(balances) / 1000000000000000000;
+		setTimeout(function() {
+			callback(balances);
+		}, 1000)
+	})
 	//var fromAddr = '0x10592A6daD0055c586bb95474e7056F72462997A'
-	var fromAddr = '0x' + plus.storage.getItem('walletAddress');
 
 	//var contraccontractAddrtAddr = '0x11769e3b12d34da9a33c1d3f08e8851a2a0528b5' // erc20 合约地址
 	//contractAddr = '0xa4d17ab1ee0efdd23edc2869e7ba96b89eecf9ab'; // TRUE  erc20 合约地址
 
-	var abi = [{
-		"constant": true,
-		"inputs": [],
-		"name": "name",
-		"outputs": [{
-			"name": "",
-			"type": "string"
-		}],
-		"payable": false,
-		"type": "function"
-	}, {
-		"constant": true,
-		"inputs": [],
-		"name": "totalSupply",
-		"outputs": [{
-			"name": "supply",
-			"type": "uint256"
-		}],
-		"payable": false,
-		"type": "function"
-	}, {
-		"constant": false,
-		"inputs": [{
-			"name": "_from",
-			"type": "address"
-		}, {
-			"name": "_to",
-			"type": "address"
-		}, {
-			"name": "_value",
-			"type": "uint256"
-		}],
-		"name": "transferFrom",
-		"outputs": [{
-			"name": "success",
-			"type": "bool"
-		}],
-		"payable": false,
-		"type": "function"
-	}, {
-		"constant": true,
-		"inputs": [],
-		"name": "decimals",
-		"outputs": [{
-			"name": "",
-			"type": "uint256"
-		}],
-		"payable": false,
-		"type": "function"
-	}, {
-		"constant": true,
-		"inputs": [{
-			"name": "_owner",
-			"type": "address"
-		}],
-		"name": "balanceOf",
-		"outputs": [{
-			"name": "balance",
-			"type": "uint256"
-		}],
-		"payable": false,
-		"type": "function"
-	}, {
-		"constant": true,
-		"inputs": [],
-		"name": "symbol",
-		"outputs": [{
-			"name": "",
-			"type": "string"
-		}],
-		"payable": false,
-		"type": "function"
-	}, {
-		"constant": false,
-		"inputs": [{
-			"name": "_to",
-			"type": "address"
-		}, {
-			"name": "_value",
-			"type": "uint256"
-		}],
-		"name": "transfer",
-		"outputs": [{
-			"name": "success",
-			"type": "bool"
-		}],
-		"payable": false,
-		"type": "function"
-	}, {
-		"constant": true,
-		"inputs": [{
-			"name": "_owner",
-			"type": "address"
-		}, {
-			"name": "_spender",
-			"type": "address"
-		}],
-		"name": "allowance",
-		"outputs": [{
-			"name": "remaining",
-			"type": "uint256"
-		}],
-		"payable": false,
-		"type": "function"
-	}, {
-		"anonymous": false,
-		"inputs": [{
-			"indexed": true,
-			"name": "_from",
-			"type": "address"
-		}, {
-			"indexed": true,
-			"name": "_to",
-			"type": "address"
-		}, {
-			"indexed": false,
-			"name": "_value",
-			"type": "uint256"
-		}],
-		"name": "Transfer",
-		"type": "event"
-	}, {
-		"anonymous": false,
-		"inputs": [{
-			"indexed": true,
-			"name": "_owner",
-			"type": "address"
-		}, {
-			"indexed": true,
-			"name": "_spender",
-			"type": "address"
-		}, {
-			"indexed": false,
-			"name": "_value",
-			"type": "uint256"
-		}],
-		"name": "Approval",
-		"type": "event"
-	}]
-
-	contract = web3.eth.contract(abi).at(contractAddr);
-	let keyStore = plus.storage.getItem('keystore');
-	setWeb3Provider(keyStore);
-	symbol = contract.symbol();
-	name = contract.name();
-	//decimals = contract.decimals(); //小数点位数
-	balances = contract.balanceOf(fromAddr); //查地址的余额	
-	balances = Number(balances) / 1000000000000000000;
-	callback();
 }
--- app-src/src/js/asset/asset.js
+++ app-src/src/js/asset/asset.js
@@ -1,7 +1,7 @@
 mui.plusReady(function() {
 	! function() {
 		h('.my-wallet').html(plus.storage.getItem('walletName'))
-		h('.walletAddress').html('0x' + plus.storage.getItem('walletAddress'))
+		h('.walletAddress').html(plus.storage.getItem('walletAddress'))
 	}();
 
 	h('.my-head').tap(function() {
@@ -16,7 +16,33 @@ mui.plusReady(function() {
 		openInfo('view/asset/newcurrency.html');
 	});
 
+	//h('.backup-mask').removeClass('mui-hidden')
 
+	//let po = plus.storage.getItem('alreadyAdded');
+	//if(po) {
+	//addToken(po, function() {
+	//	let string = `
+	//	<div class="asset-content-list">
+	//				<a href="javascript:openInfo('view/asset/currencydetail.html',{'currencyName':'${symbol}','currencyPrice':'${balances}'})">
+	//					<div class="list-left"></div>
+	//					<div class="list-middle">
+	//						<div class="title top">${symbol}</div>
+	//						<div class="price bottom"><span>0.00</span>CNY</div>
+	//					</div>
+	//					<div class="list-right">
+	//						<div class="top">${balances}</div>
+	//						<div class="price bottom"><span>0.00</span>CNY</div>
+	//					</div>
+	//				</a>
+	//			</div>`;
+	//	$('#assetContent').append(string);
+	//})
+	//};
+
+	//立即备份提示
+	//if(!!plus.storage.getItem('backupFlag')) {
+	//	h('.backup-mask').addClass('mui-hidden')
+	//}
 
 	//关闭弹窗
 	h('.close').tap(function() {
