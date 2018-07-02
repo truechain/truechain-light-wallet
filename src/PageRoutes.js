376abc4f22c17d9424c8fec27e5e1f5dc07cd85c
lijian <aimilixi@foxmail.com>
2018-07-02 09:09:45 +0800

    merge

--- src/PageRoutes.js
+++ src/PageRoutes.js
@@ -7,15 +7,12 @@ import Receipt from './components/asset/receipt'
 import Guide from './guide/guide'
 import CreateWallet from './components/my/wallet/createWallet'
 import ImportWallet from './components/my/wallet/importWallet'
-<<<<<<< HEAD
 import AboutUs from './components/my/aboutus'
 import UserPolicy from './components/my/userpolicy'
 import PrivacyPolicy from './components/my/privacypolicy'
 import Versions from './components/my/versions'
-=======
 import WalletInfo from './components/asset/walletInfo'
 import ExportMnemonic from './components/asset/exportMnemonic'
->>>>>>> 317b7d954bdbb8b946c3d8f3ac5e2279025f4ed0
 
 export default createStackNavigator(
     {
@@ -44,12 +41,10 @@ export default createStackNavigator(
         Receipt: Receipt,
         CreateWallet: CreateWallet,
         ImportWallet: ImportWallet,
-<<<<<<< HEAD
         AboutUs,
         UserPolicy,
         PrivacyPolicy,
-        Versions
-=======
+        Versions,
         WalletInfo: {
             screen: WalletInfo,
             navigationOptions: {
@@ -62,7 +57,6 @@ export default createStackNavigator(
                 headerTitle: '导出助记词'
             }
         },
->>>>>>> 317b7d954bdbb8b946c3d8f3ac5e2279025f4ed0
     },
     {
         initialRouteName: 'Guide',
