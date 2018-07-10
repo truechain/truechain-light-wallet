0ff7f5ea97569bbb997be6b706f9460b16c0f818
see91 <loverrenmin@qq.com>
2018-07-10 17:34:24 +0800

    :fire: Splash

--- app/components/public/Splash.js
+++ app/components/public/Splash.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Dimensions, Animated } from 'react-native';
-// import store from 'react-native-simple-store';
-// import { registerApp } from 'react-native-wechat';
-// import AV from 'leancloud-storage';
 import SplashScreen from 'react-native-splash-screen';
-// import NavigationUtil from '../utils/NavigationUtil';
 
 const maxHeight = Dimensions.get('window').height;
 const maxWidth = Dimensions.get('window').width;
@@ -20,36 +16,12 @@ class Splash extends React.Component {
         this.state = {
             bounceValue: new Animated.Value(1)
         };
-        // registerApp('wxb24c445773822c79');
-        // if (!AV.applicationId) {
-        //     AV.init({
-        //         appId: 'Tfi1z7dN9sjMwSul8sYaTEvg-gzGzoHsz',
-        //         appKey: '57qmeEJonefntNqRe17dAgi4'
-        //     });
-        // }
     }
 
     componentDidMount() {
         const { navigate } = this.props.navigation;
-        // Animated.timing(this.state.bounceValue, {
-        //     toValue: 1.2,
-        //     duration: 1000
-        // }).start();
         SplashScreen.hide();
-        // this.timer = setTimeout(() => {
-        //     store.get('isInit').then((isInit) => {
-        //         if (!isInit) {
-        //             navigate('Category', { isFirst: true });
-        //         } else {
-        //             NavigationUtil.reset(this.props.navigation, 'Home');
-        //         }
-        //     });
-        // }, 1000);
     }
-
-    // componentWillUnmount() {
-    //     clearTimeout(this.timer);
-    // }
 
     render() {
         return (
