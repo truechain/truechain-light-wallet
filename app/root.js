1d2b24584f278eae1838579fadf67c351982277a
baixiaojian <15101661380@163.com>
2018-07-20 19:32:54 +0800

    feat global catch

--- app/root.js
+++ app/root.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import App from './containers/app';
+require('ErrorUtils').setGlobalHandler(function (err) {
+    alert(err)
+});
 
 const Root = () => (
     <Provider store={store}>
