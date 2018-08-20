6c7b7b720489a4fc2de9c60a2f000ece27970fd7
zihuatanejp <aimilixi@gmail.com>
2018-08-20 16:53:36 +0800

    :globe_with_meridians:  about i18n support

--- language/i18n.js
+++ language/i18n.js
@@ -1,6 +1,9 @@
 import I18n, { getLanguages } from 'react-native-i18n';
-import en from './en';
-import zh from './zh';
+import en from './en';  //英文
+import zh from './zh';  //中文
+import th from './th';  //泰国
+import jp from './jp';  //日本
+
 
 I18n.defaultLocale = 'en';
 I18n.fallbacks = true;
@@ -8,7 +11,9 @@ I18n.fallbacks = true;
 // I18n.locale = 'en';
 I18n.translations = {
 	zh,
-	en
+	en,
+	th,
+	jp
 };
 
 export { I18n, getLanguages };
