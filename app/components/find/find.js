9c7703995bc0d0d72f3586a996a7ad7d28922cab
see91 <loverrenmin@qq.com>
2018-12-22 17:03:13 +0800

    find app page

--- app/components/find/find.js
+++ app/components/find/find.js
@@ -1,28 +1,135 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, StyleSheet, ScrollView,
+} from 'react-native';
+import {
+  List, ListItem, Thumbnail, Card, CardItem, Body,
+} from 'native-base';
+import { screenWidth, screenHeight } from '../../utils/Dimensions';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  view: {
+    marginTop: 10,
+    padding: 15,
+    backgroundColor: '#FFFFFF',
+    // borderWidth: 1,
+    // borderColor: 'green',
+  },
+  view_app: {
+    // borderWidth: 1,
+    // borderColor: 'red',
+  },
+  view_t: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  view_tL: {
+    fontSize: 18,
+    color: '#203260',
+  },
+  view_tR: {
+    fontSize: 12,
+    color: '#7F838D',
+  },
+  view_content: {
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+  view_list: {
+    width: screenWidth - 30,
+    // borderWidth: 1,
+    // borderColor: 'green',
+    overflow: 'hidden',
+  },
+  view_listItem: {
+    // borderWidth: 1 / 2,
+    // borderColor: 'red',
+    flexWrap: 'wrap',
+    marginLeft: 0,
+  },
+  view_avatar: {
+    // borderWidth: 1 / 2,
+    // borderColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: (screenWidth - 30) / 4,
+    height: (screenWidth - 30) / 4,
+  },
+});
+
 
 class Find extends Component {
-	constructor(props) {
-		super(props);
-		this.navigate = this.props.navigation.navigate;
-	}
+  constructor(props) {
+    super(props);
+    this.navigate = this.props.navigation.navigate;
+  }
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text>
-					发现
-				</Text>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <View style={[styles.view, styles.view_app]}>
+          <View style={styles.view_t}>
+            <Text style={styles.view_tL}>应用</Text>
+            <Text style={styles.view_tR}>更多</Text>
+          </View>
+
+          <View style={styles.view_content}>
+            <List style={styles.view_list}>
+              <ListItem avatar style={styles.view_listItem}>
+                <View style={styles.view_avatar}>
+                  <Thumbnail
+                    source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+                  />
+                  <Text>非小号</Text>
+                </View>
+                <View style={styles.view_avatar}>
+                  <Thumbnail
+                    source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+                  />
+                  <Text>非小号</Text>
+                </View>
+                <View style={styles.view_avatar}>
+                  <Thumbnail
+                    source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+                  />
+                  <Text>非小号</Text>
+                </View>
+                <View style={styles.view_avatar}>
+                  <Thumbnail
+                    source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+                  />
+                  <Text>非小号</Text>
+                </View>
+                <View style={styles.view_avatar}>
+                  <Thumbnail
+                    source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+                  />
+                  <Text>非小号</Text>
+                </View>
+                <View style={styles.view_avatar}>
+                  <Thumbnail
+                    source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+                  />
+                  <Text>非小号</Text>
+                </View>
+                <View style={styles.view_avatar}>
+                  <Thumbnail
+                    source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+                  />
+                  <Text>非小号</Text>
+                </View>
+              </ListItem>
+            </List>
+          </View>
+          <View />
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
 export default Find;
-
-const styles = StyleSheet.create({
-	container: {
-		borderWidth:1,
-		borderColor:'red'
-	}
-});
--- app/containers/app.js
+++ app/containers/app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Text, AsyncStorage } from 'react-native';
-import { I18n } from '../../language/i18n'; // 多国语言支持
 import { createStackNavigator, createBottomTabNavigator, StackNavigator } from 'react-navigation'; // 页面切换 路由导航组件
+import Storage from 'react-native-storage';
+import { I18n } from '../../language/i18n'; // 多国语言支持
 import { nodeHost, trueHost } from '../utils/config';
 
 // TabBar 底部栏位页面
 // import Splash from '../pages/Splash'; // app开屏画面
 // import Assets from '../components/asset/asset'; // 底部：资产
-import Find from '../components/find/find'; // 底部： 发现
+import Find_item from '../components/find/find'; // 底部： 发现
 import My_item from '../components/my/my'; // 底部： 我的
 
 // Router
@@ -38,14 +39,13 @@ import WebSetting from '../components/my/webSetting';
 import QRscanner from '../components/public/QRscanner';
 
 // rely
-import Storage from 'react-native-storage';
 import Icon from '../pages/iconSets';
 
 const storage = new Storage({
-	size: 1000,
-	storageBackend: AsyncStorage,
-	defaultExpires: null,
-	enableCache: true
+  size: 1000,
+  storageBackend: AsyncStorage,
+  defaultExpires: null,
+  enableCache: true,
 });
 global.storage = storage;
 
@@ -64,272 +64,294 @@ global.storage = storage;
 // 	});
 
 storage
-	.load({
-		key: 'localLanguage'
-	})
-	.then((res) => {
-		I18n.locale = res.localLanguage;
-	})
-	.catch((e) => {
-		console.log(e, '首次获取系统语言');
-	});
+  .load({
+    key: 'localLanguage',
+  })
+  .then((res) => {
+    I18n.locale = res.localLanguage;
+  })
+  .catch((e) => {
+    console.log(e, '首次获取系统语言');
+  });
 
-// const Web3 = require('web3');
-const WebTrue = require('etrue');
+const Web3 = require('web3');
 
 function check(host) {
-	if (host.includes('ropsten')) {
-		store.dispatch({
-			type: 'CONTRACTADDR',
-			TRUEContractAddr: '0x2792d677B7Ba6B7072bd2293F64BC0C1CDe23ac1',
-			TTRContractAddr: '0x635AfeB8739f908A37b3d312cB4958CB2033F456'
-		});
-	} else {
-		store.dispatch({
-			type: 'CONTRACTADDR',
-			TRUEContractAddr: '0xa4d17ab1ee0efdd23edc2869e7ba96b89eecf9ab',
-			TTRContractAddr: '0xf2bb016e8c9c8975654dcd62f318323a8a79d48e'
-		});
-	}
-	global.host = host;
-	// const web3 = new Web3(new Web3.providers.HttpProvider(host));
-	// const webtrue = new WebTrue.modules.ETrue(trueHost);
-	// const webtrue = new Web3(new Web3.providers.HttpProvider(trueHost));
-	const web3 = new WebTrue(new WebTrue.providers.HttpProvider(trueHost));
+  if (host.includes('ropsten')) {
+    store.dispatch({
+      type: 'CONTRACTADDR',
+      TRUEContractAddr: '0x2792d677B7Ba6B7072bd2293F64BC0C1CDe23ac1',
+      TTRContractAddr: '0x635AfeB8739f908A37b3d312cB4958CB2033F456',
+    });
+  } else {
+    store.dispatch({
+      type: 'CONTRACTADDR',
+      TRUEContractAddr: '0xa4d17ab1ee0efdd23edc2869e7ba96b89eecf9ab',
+      TTRContractAddr: '0xf2bb016e8c9c8975654dcd62f318323a8a79d48e',
+    });
+  }
+  global.host = host;
+  const web3 = new Web3(new Web3.providers.HttpProvider(trueHost));
 
-	global.web3 = web3;
-	// global.webtrue = webtrue;
+  global.web3 = web3;
+  // global.webtrue = webtrue;
 }
 
 storage
-	.load({
-		key: 'webHost'
-	})
-	.then(({ webHost }) => {
-		check(webHost);
-	})
-	.catch((e) => {
-		check(nodeHost);
-	});
+  .load({
+    key: 'webHost',
+  })
+  .then(({ webHost }) => {
+    check(webHost);
+  })
+  .catch((e) => {
+    check(nodeHost);
+  });
+
+
+const Find = createStackNavigator({
+  Find: {
+    screen: Find_item,
+    navigationOptions: () => ({
+      title: '发现',
+      headerBackTitle: null,
+      headerStyle: {
+        borderBottomWidth: 0,
+      },
+      headerTitleStyle: {
+        color: '#000',
+        fontSize: 18,
+      },
+    }),
+  },
+});
 
 const My = createStackNavigator({
-	My: {
-		screen: My_item,
-		navigationOptions: () => ({
-			title: I18n.t('tab.my'),
-			headerBackTitle: null,
-			headerStyle: {
-				borderBottomWidth: 0
-			},
-			headerTitleStyle: {
-				color: '#000',
-				fontSize: 18
-			}
-		})
-	}
+  My: {
+    screen: My_item,
+    navigationOptions: () => ({
+      title: I18n.t('tab.my'),
+      headerBackTitle: null,
+      headerStyle: {
+        borderBottomWidth: 0,
+      },
+      headerTitleStyle: {
+        color: '#000',
+        fontSize: 18,
+      },
+    }),
+  },
 });
 
 const TabBarPage = createBottomTabNavigator(
-	{
-		Assets: {
-			screen: Turn,
-			navigationOptions: {
-				tabBarLabel: ({ tintColor, focused }) => (
-					<Text style={{ color: tintColor, fontSize: 12, textAlign: 'center' }}>{I18n.t('tab.assets')}</Text>
-				),
-				tabBarIcon: ({ focused, tintColor }) => <Icon name="icon-qianbao1" size={30} color={tintColor} />
-			}
-		},
-		Find: {
-			screen: Find,
-			navigationOptions: {
-				tabBarLabel: ({ tintColor, focused }) => (
-					<Text style={{ color: tintColor, fontSize: 12, textAlign: 'center' }}>
-						发现
-					</Text>
-				),
-				tabBarIcon: ({ focused, tintColor }) => <Icon name="icon-faxian" size={30} color={tintColor} />
-			}
-		},
-		My: {
-			screen: My,
-			navigationOptions: {
-				tabBarLabel: ({ tintColor, focused }) => (
-					<Text style={{ color: tintColor, fontSize: 12, textAlign: 'center' }}>{I18n.t('tab.my')}</Text>
-				),
-				tabBarIcon: ({ focused, tintColor }) => <Icon name="icon-wode" size={30} color={tintColor} />
-			}
-		}
-	},
-	{
-		lazy: true,
-		animationEnabled: true,
-		backBehavior: true,
-		tabBarPosition: 'bottom',
-		tabBarOptions: {
-			activeTintColor: '#0071BC',
-			inactiveTintColor: '#999999',
-			showIcon: true,
-			style: {
-				backgroundColor: '#fff'
-			},
-			indicatorStyle: {
-				opacity: 0
-			},
-			tabStyle: {
-				padding: 0
-			}
-		}
-	}
+  {
+    Assets: {
+      screen: Turn,
+      navigationOptions: {
+        tabBarLabel: ({ tintColor, focused }) => (
+          <Text style={{ color: tintColor, fontSize: 12, textAlign: 'center' }}>{I18n.t('tab.assets')}</Text>
+        ),
+        tabBarIcon: ({ focused, tintColor }) => <Icon name="icon-qianbao1" size={30} color={tintColor} />,
+      },
+    },
+    Find: {
+      screen: Find,
+      navigationOptions: {
+        tabBarLabel: ({ tintColor, focused }) => (
+          <Text style={{ color: tintColor, fontSize: 12, textAlign: 'center' }}>发现</Text>
+        ),
+        tabBarIcon: ({ focused, tintColor }) => <Icon name="icon-faxian" size={30} color={tintColor} />,
+      },
+    },
+    My: {
+      screen: My,
+      navigationOptions: {
+        tabBarLabel: ({ tintColor, focused }) => (
+          <Text style={{ color: tintColor, fontSize: 12, textAlign: 'center' }}>{I18n.t('tab.my')}</Text>
+        ),
+        tabBarIcon: ({ focused, tintColor }) => <Icon name="icon-wode" size={30} color={tintColor} />,
+      },
+    },
+  },
+  {
+    lazy: true,
+    animationEnabled: true,
+    backBehavior: true,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      activeTintColor: '#0071BC',
+      inactiveTintColor: '#999999',
+      showIcon: true,
+      style: {
+        backgroundColor: '#fff',
+      },
+      indicatorStyle: {
+        opacity: 0,
+      },
+      tabStyle: {
+        padding: 0,
+      },
+    },
+  },
 );
 
 const App = createStackNavigator(
-	{
-		Home: {
-			screen: TabBarPage,
-			navigationOptions: {
-				header: null,
-				gesturesEnabled: false
-			}
-		},
-		CurrencyDetail,
-		Transfer,
-		Receipt: {
-			screen: Receipt,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('assets.currency.receipt')}</Text>
-			}
-		},
-		CreateWallet: {
-			screen: CreateWallet,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('wallet.creatWallet_title')}</Text>
-			}
-		},
-		ImportWallet: {
-			screen: ImportWallet,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('guide.importWallet')}</Text>
-			}
-		},
-		WalletInfo: {
-			screen: WalletInfo,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('assets.walletInfo.title')}</Text>
-			}
-		},
-		ExportMnemonic: {
-			screen: ExportMnemonic,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('assets.walletInfo.exportMnemonic')}</Text>
-			}
-		},
-		ExportKeystore: {
-			screen: ExportKeystore,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('assets.walletInfo.exportKeystore')}</Text>
-			}
-		},
-		AboutUs: {
-			screen: AboutUs,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.home.aboutUs._title')}</Text>
-			}
-		},
-		UserPolicy: {
-			screen: UserPolicy,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.home.aboutUs.useAgreement')}</Text>
-			}
-		},
-		Versions: {
-			screen: Versions,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.home.Versions._title')}</Text>
-			}
-		},
-		HelperCenter: {
-			screen: HelperCenter,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.home.helpCenter._title')}</Text>
-			}
-		},
-		FollowUs: {
-			screen: FollowUs,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.home.followUs._title')}</Text>
-			}
-		},
-		SysSet: {
-			screen: SysSet,
-			navigationOptions: {
-				headerTitle: () => <Text> {I18n.t('my.sysSetting._title')}</Text>
-			}
-		},
-		Login: {
-			screen: Login,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.home.lockAccount.loginIn')}</Text>
-			}
-		},
-		Lockpositon: {
-			screen: Lockpositon,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('public.lockedWarehouse')}</Text>
-			}
-		},
-		LockAccount: {
-			screen: LockAccount,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.home.lockAccount._title')}</Text>
-			}
-		},
-		LogOut: {
-			screen: LogOut,
-			navigationOptions: {
-				headerTitle: () => <Text> {I18n.t('my.home.lockAccount._title1')}</Text>
-			}
-		},
-		SysLanguage: {
-			screen: SysLanguage,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.sysSetting.language.multi_language')} </Text>
-			}
-		},
-		TransactionRecord: {
-			screen: TransactionRecord,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.home.transactionRecord')} </Text>
-			}
-		},
-		KnowledgePoint,
-		WebSetting: {
-			screen: WebSetting,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('my.webHost')} </Text>
-			}
-		},
-		QRscanner: {
-			screen: QRscanner,
-			navigationOptions: {
-				headerTitle: () => <Text>{I18n.t('public.scan')}</Text>
-			}
-		}
-	},
-	{
-		// initialRouteName: 'Home',
-		headerMode: 'screen',
-		navigationOptions: {
-			headerStyle: {
-				backgroundColor: '#fff'
-			},
-			headerTitleStyle: {
-				color: '#000',
-				fontSize: 18
-			},
-			headerTintColor: '#000'
-		}
-	}
+  {
+    Home: {
+      screen: TabBarPage,
+      navigationOptions: {
+        header: null,
+        gesturesEnabled: false,
+      },
+    },
+    CurrencyDetail,
+    Transfer,
+    Receipt: {
+      screen: Receipt,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('assets.currency.receipt')}</Text>,
+      },
+    },
+    CreateWallet: {
+      screen: CreateWallet,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('wallet.creatWallet_title')}</Text>,
+      },
+    },
+    ImportWallet: {
+      screen: ImportWallet,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('guide.importWallet')}</Text>,
+      },
+    },
+    WalletInfo: {
+      screen: WalletInfo,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('assets.walletInfo.title')}</Text>,
+      },
+    },
+    ExportMnemonic: {
+      screen: ExportMnemonic,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('assets.walletInfo.exportMnemonic')}</Text>,
+      },
+    },
+    ExportKeystore: {
+      screen: ExportKeystore,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('assets.walletInfo.exportKeystore')}</Text>,
+      },
+    },
+    AboutUs: {
+      screen: AboutUs,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('my.home.aboutUs._title')}</Text>,
+      },
+    },
+    UserPolicy: {
+      screen: UserPolicy,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('my.home.aboutUs.useAgreement')}</Text>,
+      },
+    },
+    Versions: {
+      screen: Versions,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('my.home.Versions._title')}</Text>,
+      },
+    },
+    HelperCenter: {
+      screen: HelperCenter,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('my.home.helpCenter._title')}</Text>,
+      },
+    },
+    FollowUs: {
+      screen: FollowUs,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('my.home.followUs._title')}</Text>,
+      },
+    },
+    SysSet: {
+      screen: SysSet,
+      navigationOptions: {
+        headerTitle: () => (
+          <Text>{I18n.t('my.sysSetting._title')}</Text>
+        ),
+      },
+    },
+    Login: {
+      screen: Login,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('my.home.lockAccount.loginIn')}</Text>,
+      },
+    },
+    Lockpositon: {
+      screen: Lockpositon,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('public.lockedWarehouse')}</Text>,
+      },
+    },
+    LockAccount: {
+      screen: LockAccount,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('my.home.lockAccount._title')}</Text>,
+      },
+    },
+    LogOut: {
+      screen: LogOut,
+      navigationOptions: {
+        headerTitle: () => (
+          <Text>{I18n.t('my.home.lockAccount._title1')}</Text>
+        ),
+      },
+    },
+    SysLanguage: {
+      screen: SysLanguage,
+      navigationOptions: {
+        headerTitle: () => (
+          <Text>{I18n.t('my.sysSetting.language.multi_language')}</Text>
+        ),
+      },
+    },
+    TransactionRecord: {
+      screen: TransactionRecord,
+      navigationOptions: {
+        headerTitle: () => (
+          <Text>{I18n.t('my.home.transactionRecord')}</Text>
+        ),
+      },
+    },
+    KnowledgePoint,
+    WebSetting: {
+      screen: WebSetting,
+      navigationOptions: {
+        headerTitle: () => (
+          <Text>{I18n.t('my.webHost')}</Text>
+        ),
+      },
+    },
+    QRscanner: {
+      screen: QRscanner,
+      navigationOptions: {
+        headerTitle: () => <Text>{I18n.t('public.scan')}</Text>,
+      },
+    },
+  },
+  {
+    // initialRouteName: 'Home',
+    headerMode: 'screen',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#fff',
+      },
+      headerTitleStyle: {
+        color: '#000',
+        fontSize: 18,
+      },
+      headerTintColor: '#000',
+    },
+  },
 );
 
 export default App;
